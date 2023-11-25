e525096d5b0282a81a75833b546c5ae7e7324720
MarcusWheeler <wheemarc@oregonstate.edu>
2023-11-25 13:23:29 -0800

    You now spawn directly at the Battle Factory (Janky :/)

--- src/new_game.c
+++ src/new_game.c
@@ -126,7 +126,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
+    SetWarpDestination(MAP_GROUP(BATTLE_FRONTIER_BATTLE_FACTORY_LOBBY), MAP_NUM(BATTLE_FRONTIER_BATTLE_FACTORY_LOBBY), WARP_ID_NONE, -1, -1);
     WarpIntoMap();
 }
 
