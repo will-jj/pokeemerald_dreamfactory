3498b19ac6af4619db3cf8d0e442e81b2a070830
William Jackson <will-jj@users.noreply.github.com>
2024-12-28 21:29:37 +0000

    Add version string to main menu
    Setup as done in Emerald Rogue, preffix, & suffix available but currently blank

---
+++ src/data/dream_strings.h
@@ -0,0 +1,6 @@
+// Version strings for dream factory
+const u8 gText_DreamFactoryVersion[] = _("v1.6.0");
+
+const u8 gText_DreamFactoryVersionSpacer[] = _("");
+const u8 gText_DreamFactoryVersionPrefix[] = _("");
+const u8 gText_DreamFactoryVersionSuffix[] = _("");
